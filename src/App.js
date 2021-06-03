1a9f0fee1e02d821609e931bc29f9a9a0e8f6470
GustiAmelia <gusti_amelia@apps.ipb.ac.id>
2021-06-04 02:24:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='card'>
+        <div className='card-content'>
+          <div className='header'>
+            <div className='title-wrapper'>
+              <h1>HI !</h1>
+              <h2>Your Account is Verified</h2>
+            </div>
+            <div className='image-wrapper'>
+              <img src='https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg' alt='...'/>
+            </div>
+          </div>   
+          <div className='content'>
+            <p>Laborum laborum amet irure et nulla ad dolor cupidatat aliqua voluptate eu eiusmod qui. Deserunt nostrud do officia occaecat consectetur. Dolor exercitation fugiat ad dolore nisi cillum irure ut elit anim deserunt. Sint irure fugiat laborum amet incididunt est id.
+
+Sit proident magna duis Lorem voluptate esse exercitation labore aute ipsum minim. Dolor ea dolor fugiat eiusmod irure anim ullamco. Anim proident incididunt ad dolor ipsum id dolore ipsum. Aliquip tempor deserunt ipsum laboris. Exercitation esse ad veniam consectetur quis nisi adipisicing enim dolor irure amet. Dolor non duis adipisicing dolore nostrud ea veniam.
+
+Do anim id cillum ad commodo laboris anim excepteur dolor voluptate proident. Ipsum elit nostrud magna Lorem voluptate anim eu magna esse. Officia irure laborum proident ullamco veniam voluptate labore duis. Aliquip nostrud adipisicing eu minim adipisicing quis amet do commodo exercitation pariatur nostrud dolor dolor. Adipisicing labore magna est adipisicing. Deserunt aliqua et magna voluptate. Amet fugiat enim aute aliqua in dolor sit consectetur elit commodo magna officia incididunt nisi.</p>
+          </div>         
+        </div>
+        
+      </div>
     </div>
   );
 }
